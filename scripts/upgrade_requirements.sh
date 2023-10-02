332d77d5efdb1fdf61608d845f26a040c28c464b
irtazaakram <irtaza.akram@arbisoft.com>
2023-10-02 15:14:37 +0500

    fix: version

--- scripts/upgrade_requirements.sh
+++ scripts/upgrade_requirements.sh
@@ -7,18 +7,9 @@ pip-compile coreapi_coreschema.in "$@" --upgrade
 pip-compile debug.in "$@" --upgrade
 pip-compile deployment.in "$@" --upgrade
 pip-compile dev.in "$@" --upgrade
-pip-compile django_2_2.in "$@" --upgrade
-pip-compile django_2_2_and_elastic_6x.in "$@" --upgrade
-pip-compile django_2_2_and_elastic_7x.in "$@" --upgrade
-pip-compile django_3_1.in "$@" --upgrade
-pip-compile django_3_1_and_elastic_6x.in "$@" --upgrade
-pip-compile django_3_1_and_elastic_7x.in "$@" --upgrade
 pip-compile django_3_2.in "$@" --upgrade
 pip-compile django_3_2_and_elastic_6x.in "$@" --upgrade
 pip-compile django_3_2_and_elastic_7x.in "$@" --upgrade
-pip-compile django_4_0.in "$@" --upgrade
-pip-compile django_4_0_and_elastic_6x.in "$@" --upgrade
-pip-compile django_4_0_and_elastic_7x.in "$@" --upgrade
 pip-compile django_4_1.in "$@" --upgrade
 pip-compile django_4_1_and_elastic_6x.in "$@" --upgrade
 pip-compile django_4_1_and_elastic_7x.in "$@" --upgrade
@@ -30,5 +21,6 @@ pip-compile documentation.in "$@" --upgrade
 pip-compile elastic.in "$@" --upgrade
 pip-compile elastic_6x.in "$@" --upgrade
 pip-compile elastic_7x.in "$@" --upgrade
+pip-compile elastic_docker.in "$@" --upgrade
 pip-compile test.in "$@" --upgrade
 pip-compile testing.in "$@" --upgrade
